d91ef9cb11ce3cbc3beb0c478d142b6ea2348dd7
Leandretee <159179084+Leandretee@users.noreply.github.com>
2024-08-19 12:01:34 -0400

    ok

---
+++ Introduccion/Python/Python.py
@@ -0,0 +1,15 @@
+def Hola_Mundo():
+    print("Hola Mundo")
+
+def Obtener_Edad():
+    edad = input("Ingrese su edad: ")
+    print("Tu edad es: ", edad)
+
+def Calcular_Edad():
+    anioNacimiento = int(input("Ingresa el año de nacimiento:"))
+    edad = 2024 - anioNacimiento
+    print("Su edad calculada es: ", edad)
+
+Hola_Mundo()
+Obtener_Edad()
+Calcular_Edad()

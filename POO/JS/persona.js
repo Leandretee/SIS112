f024e5f6a7100900afbf85621ea9c12eafbe2122
Leandretee <159179084+Leandretee@users.noreply.github.com>
2024-08-29 21:01:37 -0400

    js

--- POO/JS/persona.js
+++ POO/JS/persona.js
@@ -21,6 +21,61 @@ class Persona {
     Universidad() {
       return 'Mi universidad es la Catolica de Santa Cruz ' + this.universidad;
     }
+
+    modificar_nombre() {
+        this.nombre = prompt("Ingresar tu nuevo nombre")
+        while (!this.nombre ||!isNaN(this.nombre)) {
+            alert("Nombre invalido, Coloque letras para su nombre porfavor");
+            this.nombre = prompt("Ingrese nuevamente el nombre");
+        }
+        return 'Hola, mi nuevo nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
+      }
+    
+    modificar_cumpleanios() {
+        this.edad = prompt ('ingrese su nueva edad');
+    while (this.edad  <=0){
+        alert("La edad es incorrecta. Ingrese una edad válida");
+      ;
+    }
+    if (this.edad < 0) {
+      this.edad = prompt ('ingrese nuevamente su edad')
+      return 'Ahora tengo ' + this.edad + ' años ';
+    }
+      }
+    
+    modificar_estudiar() {
+        this.carrera = prompt("Ingresar su nueva carrera")
+        while (!this.carrera || !isNaN(this.carrera)) {
+            alert("Carrera invalida, Coloque letras para su carrera");
+            this.carrera = prompt("Ingrese nuevamente su carrera");
+        }
+        return 'Ahora estudio ' + this.carrera;
+      }
+  
+    modificar_Universidad() {
+        this.universidad = prompt("Ingresar su nueva universidad")
+        while (!this.universidad || !isNaN(this.universidad)) {
+            alert("Universidad invalida, coloque letras porfavor");
+            this.universidad = prompt("Ingrese nuevamente su universidad");
+        }
+        return 'Mi nueva universidad es ' + this.universidad;
+      }
+
+    eliminar_nombre() {
+        delete 'Hola, mi nuevo nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
+    }
+
+    eliminar_cumpleanios() {
+        delete '¡Felicidades! ahora tienes ' + this.edad + ' años.'
+    }
+
+    eliminar_estudiar() {
+        delete 'Ahora estudio ' + this.carrera
+    }
+
+    eliminar_universidad() {
+        delete 'Mi nueva universidad es ' + this.universidad
+    }
   }
   
   // Crear una instancia de la clase Persona
@@ -47,6 +102,38 @@ class Persona {
     carrera.textContent = persona.estudiar();
   }
 
-  function botonUniveridad() {
-    universidad.textContent = persona.universidad();
+  function botonUniversidad() {
+    universidad.textContent = persona.Universidad();
+  }
+
+  function botonModificar_Saludo() {
+    saludar.textContent = persona.modificar_nombre();
+  }
+
+  function botonModificar_Cumpleanios() {
+    edad.textContent = persona.modificar_cumpleanios();
+  }
+
+  function botonModificar_Estudiar() {
+    carrera.textContent = persona.modificar_estudiar();
+  }
+
+  function botonModificar_Universidad() {
+    universidad.textContent = persona.modificar_Universidad();
+  }
+
+  function botonEliminar_Saludo(){
+    saludar.textContent = persona.eliminar_nombre();
+  }
+
+  function botonEliminar_Cumpleanios(){
+    edad.textContent = persona.eliminar_cumpleanios();
+  }
+
+  function botonEliminar_Estudiar(){
+    carrera.textContent = persona.eliminar_estudiar();
+  }
+
+  function botonEliminar_Universidad(){
+    universidad.textContent = persona.eliminar_universidad();
   }

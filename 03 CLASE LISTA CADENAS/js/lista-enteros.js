64c3874b13fc44d14c3526b19b3f3e3f9651e8d9
Leandretee <leandro.llorenti@ucb.edu.bo>
2024-10-04 10:10:00 -0400

    .

--- 03 CLASE LISTA CADENAS/js/lista-enteros.js
+++ 03 CLASE LISTA CADENAS/js/lista-enteros.js
@@ -39,6 +39,71 @@ class ListaEnteros {
 }
 
 const miLista = new ListaEnteros();
+miLista.agregar(21);
+miLista.agregar(12);
+miLista.agregar(15);
+miLista.agregar(30);
+console.log(miLista.lista); //Imprime: [21,12,15,30]
+
+var a = miLista.lista[0];
+console.log(a);
+
+var b = miLista.lista[1];
+console.log(b);
+
+var c = miLista.lista[2];
+console.log(c);
+
+var d = miLista.lista[3];
+console.log(d);
+
+// RETORNAR EL ELEMENTO MENOR DE LA LISTA,
+/*
+var datoMenor = -1;
+
+var a = miLista.lista[0];
+if(a > datoMenor){
+    datoMenor = a;
+}
+console.log("datoMenor:" + datoMenor);
+//-------------------------------------------------------------------------------
+var b = miLista.lista[1];
+if(a > b){
+    datoMenor = b;
+}
+console.log("Dato Menor:" + datoMenor);
+//------------------------------------------------------------------------------
+var c = miLista.lista[2];
+if(b > c){
+    datoMenor = c;
+}
+console.log("Dato Menor:" + datoMenor);
+//--------------------------------------------------------------------------------
+var d = miLista.lista[3];
+if(c > d){
+    datoMenor = d;
+}
+console.log("Dato Menor:" + datoMenor);
+//--------------------------------------------------------------
+*/
+
+var datoMenor = miLista.lista[0];
+for (var i = 1; i < miLista.lista.length; i++) {
+    if (miLista.lista[i] < datoMenor) {
+        datoMenor = miLista.lista[i];
+    }
+}
+console.log("Dato Menor:" + datoMenor);
+//----------------------------------------------------------------------
+
+var datoMayor = miLista.lista[0];
+
+for (var i = 1; i < miLista.lista.length; i++) {
+    datoMayor = datoMayor > miLista.lista[i] ? datoMayor : miLista.lista[i];
+}
+
+console.log("Dato Mayor: " + datoMayor);
+//----------------------------------------------------------------------
 
 document.getElementById('agregarBtn').addEventListener('click', () => {
     const valor = parseInt(document.getElementById('numero').value);
